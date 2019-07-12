3be06f623c9c744ed937602fb464a01f426a3353
Jasper Boot <boot@datlinq.com>
2019-07-12 11:42:51 +0200

    export functions

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { None } from './none';
 export { Optional } from './optional';
 export { Some } from './some';
 export { NoSuchElementException } from './no-such-element-exception';
+export * from './functions';
